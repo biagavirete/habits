9c5f3867ee67292cdb07a57c39ac76271b273f34
Bia Gavira <biagavirete@gmail.com>
2023-01-20 13:42:58 -0300

    feat: create progress and modal components

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,43 +1,44 @@
 import { generateDateFromYearBeginning } from "../utils/utils";
+
 import HabitDay from "./HabitDay";
-
 const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
-
 const summaryDates = generateDateFromYearBeginning();
-
 const minimumSummaryDatesLength = 18 * 7;
-
 const amountOfDaysToDisplay = minimumSummaryDatesLength - summaryDates.length;
 
 function SummaryTable() {
-
   return (
     <div className="w-full flex">
-      <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {weekDays.map((day, index) => (
-          <div
-            key={`${day}-${index}`}
-            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            {day}
-          </div>
-        ))}
-      </div>
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {summaryDates.map(date => (
-          <HabitDay key={date.toString()} />
-        ))}
-
-        {amountOfDaysToDisplay > 0 &&
-          Array.from({ length: amountOfDaysToDisplay })
-            .map((_, index) => (
-              <div
-                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-                key={index}
-              />
-            ))}
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          );
+        })}
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => {
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          );
+        })}
+        {amountOfDaysToDisplay > 0 && Array.from({ length: amountOfDaysToDisplay }).map((_, i) => {
+          return (
+            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+          );
+        })}
       </div>
     </div>
   );
-}
+};
 
 export default SummaryTable;
